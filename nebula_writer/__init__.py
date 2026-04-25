c5b0d979c53c795f727b18b29914b56e30d831d3
sagar0163 <sagarjadhav063@gmail.com>
2026-04-25 22:34:38 +0530

    feat: upgrade __init__.py (Phase 2)

---
+++ nebula_writer/__init__.py
@@ -0,0 +1,9 @@
+"""
+Nebula-Writer Version
+"""
+
+__version__ = "0.1.0"
+__author__ = "Sagar"
+__description__ = "AI-Powered Fiction Writing Assistant with Persistent Memory"
+
+VERSION_INFO = {"version": __version__, "major": 0, "minor": 1, "patch": 0, "release": "alpha"}
